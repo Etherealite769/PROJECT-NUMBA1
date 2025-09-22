242ea5ba954f265b955258e98f4a8540794280d5
Etherealite769 <cubiangd93@gmail.com>
2025-09-22 15:53:07 +0800

    feature(app_name): complete app setup

--- user_input/app_name/urls.py
+++ user_input/app_name/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("delete/<int:user_id>/", views.delete_user, name="delete_user"),
 ]
--- user_input/app_name/views.py
+++ user_input/app_name/views.py
@@ -1,6 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import UserInput
+from .forms import UserInputForm
 
 def home(request):
-    return render(request, "home.html")
+    if request.method == "POST":
+        form = UserInputForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("home")
+    else:
+        form = UserInputForm()
+
+    users = UserInput.objects.all()
+    return render(request, "home.html", {"form": form, "users": users})
+
+def delete_user(request, user_id):
+    user = get_object_or_404(UserInput, id=user_id)
+    user.delete()
+    return redirect("home")
+
 
 # Create your views here.
